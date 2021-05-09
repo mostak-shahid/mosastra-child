c693a29caf5c9d9bc67368087302e5b21d38574a
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-05-09 22:45:14 +0600

    update

--- functions.php
+++ functions.php
@@ -44,6 +44,7 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+require_once 'aq_resizer.php';
 //require_once 'astra-custom.php';
 require_once 'hooks.php';
 require_once 'shortcodes.php';
