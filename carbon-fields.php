e0310238c27439975c38b725507870539a6b226a
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-05-19 13:02:31 +0600

    Update carbon-fields.php

--- carbon-fields.php
+++ carbon-fields.php
@@ -40,13 +40,15 @@ function crb_attach_theme_options() {
                     <div class="img-part"><?php echo wp_get_attachment_image( $fields['mos-image-media'], 'full' ); ?></div>
                 <?php endif?>
                 <div class="text-part">
-                    <h4><?php echo esc_html( $fields['mos-image-heading'] ); ?></h4>
-                <?php if ($fields['mos-image-content']) :?>
-                    <div class="desc"><?php echo apply_filters( 'the_content', $fields['mos-image-content'] ); ?></div> 
-                <?php endif?>                 
-                <?php if ($fields['mos-image-btn-title'] && $fields['mos-image-btn-url']) :?>   
-                    <div class="wp-block-buttons"><div class="wp-block-button"><a href="<?php echo esc_url( $fields['mos-image-btn-url'] ); ?>" title="" class="wp-block-button__link"><?php echo esc_html( $fields['mos-image-btn-title'] ); ?></a></div></div>  
-                <?php endif?>                 
+                    <?php if ($fields['mos-image-heading']) : ?>
+                        <h4><?php echo esc_html( $fields['mos-image-heading'] ); ?></h4>
+                    <?php endif?>                 
+                    <?php if ($fields['mos-image-content']) :?>
+                        <div class="desc"><?php echo apply_filters( 'the_content', $fields['mos-image-content'] ); ?></div> 
+                    <?php endif?>                 
+                    <?php if ($fields['mos-image-btn-title'] && $fields['mos-image-btn-url']) :?>   
+                        <div class="wp-block-buttons"><div class="wp-block-button"><a href="<?php echo esc_url( $fields['mos-image-btn-url'] ); ?>" title="" class="wp-block-button__link"><?php echo esc_html( $fields['mos-image-btn-title'] ); ?></a></div></div>  
+                    <?php endif?>                 
                 </div>
             </div>
         </div>
@@ -78,14 +80,14 @@ function crb_attach_theme_options() {
                 <?php endif;?>
                 <div class="text-part">
                     <?php if ($fields['mos-icon-heading']) : ?>
-                    <h4><?php echo esc_html( $fields['mos-icon-heading'] ); ?></h4>
+                        <h4><?php echo esc_html( $fields['mos-icon-heading'] ); ?></h4>
                     <?php endif;?>
                     <?php if ($fields['mos-icon-content']) : ?>
-                    <div class="desc"><?php echo  $fields['mos-icon-content']; ?></div>                    
+                        <div class="desc"><?php echo  $fields['mos-icon-content']; ?></div>                    
                     <?php endif;?>                 
-                <?php if ($fields['mos-icon-btn-title'] && $fields['mos-icon-btn-url']) :?>   
-                    <div class="wp-block-buttons"><div class="wp-block-button"><a href="<?php echo esc_url( $fields['mos-icon-btn-url'] ); ?>" title="" class="wp-block-button__link"><?php echo esc_html( $fields['mos-icon-btn-title'] ); ?></a></div></div>  
-                <?php endif?> 
+                    <?php if ($fields['mos-icon-btn-title'] && $fields['mos-icon-btn-url']) :?>   
+                        <div class="wp-block-buttons"><div class="wp-block-button"><a href="<?php echo esc_url( $fields['mos-icon-btn-url'] ); ?>" title="" class="wp-block-button__link"><?php echo esc_html( $fields['mos-icon-btn-title'] ); ?></a></div></div>  
+                    <?php endif?> 
                 </div>
             </div>
         </div>
@@ -115,14 +117,14 @@ function crb_attach_theme_options() {
                 <?php endif;?>
                 <div class="text-part">
                     <?php if ($fields['mos-svg-heading']) : ?>
-                    <h4><?php echo esc_html( $fields['mos-svg-heading'] ); ?></h4>
+                        <h4><?php echo esc_html( $fields['mos-svg-heading'] ); ?></h4>
                     <?php endif;?>
                     <?php if ($fields['mos-svg-content']) : ?>
-                    <div class="desc"><?php echo  $fields['mos-svg-content']; ?></div>                    
+                        <div class="desc"><?php echo  $fields['mos-svg-content']; ?></div>                    
                     <?php endif;?>                 
-                <?php if ($fields['mos-svg-btn-title'] && $fields['mos-svg-btn-url']) :?>   
-                    <div class="wp-block-buttons"><div class="wp-block-button"><a href="<?php echo esc_url( $fields['mos-svg-btn-url'] ); ?>" title="" class="wp-block-button__link"><?php echo esc_html( $fields['mos-svg-btn-title'] ); ?></a></div></div>  
-                <?php endif?> 
+                    <?php if ($fields['mos-svg-btn-title'] && $fields['mos-svg-btn-url']) :?>   
+                        <div class="wp-block-buttons"><div class="wp-block-button"><a href="<?php echo esc_url( $fields['mos-svg-btn-url'] ); ?>" title="" class="wp-block-button__link"><?php echo esc_html( $fields['mos-svg-btn-title'] ); ?></a></div></div>  
+                    <?php endif?> 
                 </div>
             </div>
         </div>
