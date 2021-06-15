af7d8b959007e281268733b1cbd8355862cd5fed
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-06-15 11:52:25 +0600

    Update carbon-fields.php

--- carbon-fields.php
+++ carbon-fields.php
@@ -321,7 +321,7 @@ function crb_attach_theme_options() {
                                                 $n = 0;
                                                 foreach($categories as $category) {
                                                     //echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a>';
-                                                    if (!$n) echo ', ';
+                                                    if ($n) echo ', ';
                                                     echo esc_html( $category->name );
                                                     $n++;                                                    
                                                 }
@@ -339,7 +339,7 @@ function crb_attach_theme_options() {
                                                 if ( $post_tags ) {
                                                     $n = 0;
                                                     foreach( $post_tags as $tag ) {                                                    
-                                                        if (!$n) echo ', ';
+                                                        if ($n) echo ', ';
                                                         echo esc_html( $tag->name );
                                                         $n++;
                                                     }
